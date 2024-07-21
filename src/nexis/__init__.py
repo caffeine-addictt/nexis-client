8d7b07ea633efb34c2502f80c9b264a4097f81cb
AlexNg <contact@ngjx.org>
2024-07-22 01:38:58 +0800

    feat: Add __init__
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ src/nexis/__init__.py
@@ -0,0 +1,14 @@
+"""
+# Nexis CLI
+
+The client for NYP InfoSec Auth Server
+
+CLI: https://github.com/caffeine-addictt/nexis-client
+Server: https://github.com/caffeine-addictt/nexis
+
+---
+
+Released under the GPL-3.0-only License
+
+Copyright (c) 2024, Jun Xiang. All rights reserved.
+"""
